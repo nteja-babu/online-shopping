c49d1ef69a8b83e560e7dd32ced2a9ca8c843f42
nteja-babu <133639392+tejanoolu19@users.noreply.github.com>
2025-03-01 17:41:18 +0530

    added with js path and datafile

---
+++ Data_js.js
@@ -0,0 +1,50 @@
+const records = [
+    {
+        title: " post",
+        lead: "First post content goes here. Inform readers about what this post is about."
+    },
+    {
+        title: "Title of the second blog post",
+        lead: "Second post content goes here. More information for readers about this post."
+    },
+    {
+        title: "Title of the third blog post",
+        lead: "Third post content goes here. A brief summary of this post for readers."
+    },
+    {
+        title: "Title of the fourth blog post",
+        lead: "Fourth post content goes here. An overview of the fourth post."
+    },
+    {
+        title: "Title of the fifth blog post",
+        lead: "Fifth post content goes here. A description or summary of the fifth blog post."
+    },
+    {
+        title: "Hi tejat",
+        lead: "Fifth sample content goes here. A description or summary of the fifth blog post."
+    }
+];
+
+const card_content = [
+    {
+        IMG: " https://m.media-amazon.com/images/I/41nbWPrsdDL._SX300_SY300_QL70_FMwebp_.jpg",
+        HEADING: "Heading",
+        MATTER: "Some representative placeholder content for the three columns of text below the carousel. This is the first column.",
+        PATH: 'https://amzn.to/3DeVRVK'
+    },
+    {
+        IMG: "https://raw.githubusercontent.com/nteja-babu/smart_data/refs/heads/main/my_pics/hq720.jpg",
+        HEADING: "Heading",
+        MATTER: "Some representative placeholder content for the three columns of text below the carousel. This is the first column.",
+        PATH: 'https://amzn.to/41p0swm'
+    },
+    {
+        IMG: "https://raw.githubusercontent.com/nteja-babu/smart_data/refs/heads/main/my_pics/hq720.jpg",
+        HEADING: "Heading",
+        MATTER: "Some representative placeholder content for the three columns of text below the carousel. This is the first column.",
+        PATH: 'https://amzn.to/41p0swm'
+    },
+    
+];
+
+export { records, card_content };
