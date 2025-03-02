4b22c03a1b72ecfcf3c3445c28289638a19834bf
nteja-babu <133639392+tejanoolu19@users.noreply.github.com>
2025-03-02 12:35:12 +0530

    update devotional page heading centence image

--- Data_js.js
+++ Data_js.js
@@ -3,11 +3,42 @@
 /* used for Devotional.HTML page */
 
 const records = [
+
     {
-        IMG: "https://m.media-amazon.com/images/I/71U3iznUAIL._SX425_.jpg",
-        HEADING: "Devotional",
+        IMG: "https://m.media-amazon.com/images/I/81RFhVKODxL._SX569_.jpg",
+        HEADING: "HEM Roseline Dhoop Sticks",
+        MATTER: "Bambooless & Charcoal-Free | Natural Rose Aroma for Pooja, Meditation & Spiritual Practices | Long-Lasting Fragrance | Approx. 20 Sticks | 50g Pack | Burns 35-40 Min",
+        PATH: 'https://amzn.to/4h7aUyC'
+    },
+    {
+        IMG: "https://m.media-amazon.com/images/I/61mxk0Mt9lL._SX569_.jpg",
+        HEADING: "Shyam Charan Premium Natural Wet Dhoop Cones",
+        MATTER: "Shyam Charan Premium Natural Wet Dhoop Cones with Mogra Fragrance, Pooja Bati Home Fragrance Non Toxic Herbal Dhup Batti for Pooja, Meditation, and Daily Use (Mogra) Pack of 4 (35 gm Each Box)",
+        PATH: 'https://amzn.to/43kqKT3'
+    },
+    {
+        IMG: "https://m.media-amazon.com/images/I/211mv1IO+5L._SX300_SY300_.jpg",
+        HEADING: "Silver Shine, Instant Cleaner 50ml",
+        MATTER: "Pitambari Rooperi Silver Shine, Instant Cleaner 50ml ",
+        PATH: 'https://amzn.to/3QIE2l4'
+    },
+    {
+        IMG: "https://m.media-amazon.com/images/I/316VTK1SDgL.jpg",
+        HEADING: "500 Gram Wooden Palm Fresh Original Mango Wood Sticks",
+        MATTER: "500 Gram Wooden Palm Fresh Original Mango Wood Sticks| Aam Ki Lakdi for Havan Pooja Samagri",
+        PATH: 'https://amzn.to/41CL5lh'
+    },
+    {
+        IMG: "https://m.media-amazon.com/images/I/61p8gOZa-lL._SX569_.jpg",
+        HEADING: "V, VIDHAN Dhoop Sticks Premium Jar 100Gm",
         MATTER: "Some representative placeholder content for the three columns of text below the carousel. This is the first column.",
-        PATH: 'https://amzn.to/41p0swm'
+        PATH: 'https://amzn.to/4hWvenq'
+    },
+    {
+        IMG: "https://m.media-amazon.com/images/I/71oiVVCxwnL._SX569_.jpg",
+        HEADING: "Bloomico Putta Mannu for Pooja - 100 gm",
+        MATTER: "Bloomico Putta Mannu for Pooja - 100 gm | Natural Pure Ant Burrow Soil & Snake Burrow Soil for Healing (100 Grams)",
+        PATH: 'https://amzn.to/4blK844'
     },
 ];
 /*-------------------------------------------------------------------------------*/
@@ -26,10 +57,10 @@ const ele_content = [
 /* used for Gadget.HTML page */
 
 const gad_content = [
-    
-{
+
+    {
         IMG: " https://www.amazon.in/Counzy-Planet-Crystal-Wooden-Decorations/dp/B0DPDT3639/ref=mp_s_a_1_2_sspa?crid=1M8D185C2X27F&dib=eyJ2IjoiMSJ9.7Ik5mx64Xd-G6u4EJyrok7IQkATJZs5HIVV04NWSuVYZITNf4XKKX2tEAzr2uYwa8luEz01wgbfkQtOovX3wZYX1tQHTPc_jkfECzJdijuYiQeR6zEpoo5Q-tfaBBZWS7m9oofzPOQbHYnAhe1gYAi93L1PRsre5BFhbDQqvt7W8U3yrCIx6WNHBleEmjSPr5M-DdtufYc9_M4zDV03evA.SSHS1v3mRnLprVkYgyAJH046hqj_QCduLorOyDbbYOQ&dib_tag=se&keywords=3d+boll&qid=1740897564&sprefix=3d+bol%2Caps%2C478&sr=8-2-spons&sp_csd=d2lkZ2V0TmFtZT1zcF9waG9uZV9zZWFyY2hfYXRm&psc=1#"
-,
+        ,
         HEADING: "Devotional",
         MATTER: ". One94Store 3D Galaxy Crystal Ball Night Lamp - Engraved USB Table LED Wooden Light for Home Office Decoration and Birthday Gift (Galaxy)(Warm White)",
         PATH: 'https://amzn.to/3F2VYnR '
@@ -57,7 +88,8 @@ const card_content = [
         IMG: " https://m.media-amazon.com/images/I/41ToUyhcYpL._SX300_SY300_QL70_FMwebp_.jpg",
         HEADING: "index updated from mobile",
         MATTER: "SUPPLY6 Stress Free Gummies with world's strongest KSM-66 Ashwagandha | Sleep Quality, Stress Relief, Boost Performance | Mixed Berry, Plant Based, Gluten-Free & Gelatin Free, 60 Gummies",
-        PATH: 'https://amzn.to/3QDCt7V'    },
+        PATH: 'https://amzn.to/3QDCt7V'
+    },
     {
         IMG: "https://m.media-amazon.com/images/I/71U3iznUAIL._SX425_.jpg",
         HEADING: "Heading",
@@ -76,7 +108,7 @@ const card_content = [
         MATTER: "Some representative placeholder content for the three columns of text below the carousel. This is the first column.",
         PATH: 'https://amzn.to/41p0swm'
     },
-    
+
 ];
 
-export { records, card_content, ele_content, gad_content, clo_content};
+export { records, card_content, ele_content, gad_content, clo_content };
